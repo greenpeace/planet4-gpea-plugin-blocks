80dbdfdc53c45a80955b71b86a0a6a4ebb6d4a4f
Oscar Keur <o.e.keur@gmail.com>
2018-10-01 11:18:37 +0200

    Faulty animation fixed
    The toggle animation was also triggered when the donate or sharebutton was hovered, which showed the form again. Now it stays hidden, where it should be.

--- includes/assets/js/docReady.js
+++ includes/assets/js/docReady.js
@@ -7,7 +7,7 @@
 		post_form_value["action"] = "petition_form_process";
 		post_form_value["nonce"] = petition_form_object.nonce;
 
-		var cardfront = $('.gpnl-petition');
+		cardfront = $('.gpnl-petition');
 
 		// Disable the form so people can't resubmit
 		toggleDisable('#gpnl-petitionform *');
@@ -27,7 +27,7 @@
 				flip('.gpnl-petition');
 				cardfront.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',   
 				function(e) {
-					$('.gpnl-petition-form').toggle();
+					$('.gpnl-petition-form').hide();
 				});
 			},
 			error: function(jqXHR, textStatus, errorThrown, data, url){
