a16e11058509a196b5ed457ecce0f185a5e589e9
Oscar Keur <o.e.keur@gmail.com>
2018-10-23 13:43:09 +0200

    renamed some of the GPNL block labels + changed some icons to reflect changes

--- classes/controller/blocks/class-noindex-controller.php
+++ classes/controller/blocks/class-noindex-controller.php
@@ -34,7 +34,7 @@ if ( ! class_exists( 'Petition_Controller' ) ) {
 
 			// Define the Shortcode UI arguments.
 			$shortcode_ui_args = array(
-				'label'         => __( 'Hide Page', 'planet4-gpnl-blocks' ),
+				'label'         => __( 'GPNL | Hide Page', 'planet4-gpnl-blocks' ),
 				'listItemImage' => '<img src="' . esc_url( plugins_url() . '/planet4-gpnl-plugin-blocks/admin/images/icon_noindex.png' ) . '" />',
 				'attrs'         => $fields,
 				'post_type'     => P4NLBKS_ALLOWED_PAGETYPE,
