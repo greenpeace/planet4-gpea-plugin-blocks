ef0bbedc35d018aaa65620d426d3dc503bdfb268
Oscar Keur <o.e.keur@gmail.com>
2018-10-25 16:45:33 +0200

    working on data processing

--- includes/assets/js/donationform.js
+++ includes/assets/js/donationform.js
@@ -33,7 +33,7 @@ function iDealToggle(data) {
    if ($('#frequency option:selected')[0].value === "E"){
 	   $('.wizard-footer-right > span > button').attr('onClick', 'removeIdealBtn();');
 	   $('.wizard-footer-right > span > button').text("Eenmalige machtiging");
-	   $('.wizard-footer-right').prepend('<span id="iDealBtn" role="button" tabindex="0"><button tabindex="-1" type="button" class="wizard-btn" style="background-color: rgb(238, 86, 45); border-color: rgb(238, 86, 45); color: white;" onclick="idealTransaction('+data+')">iDeal</button></span>');
+	   $('.wizard-footer-right').prepend('<span id="iDealBtn" role="button" tabindex="0"><button tabindex="-1" type="button" class="wizard-btn" style="background-color: rgb(238, 86, 45); border-color: rgb(238, 86, 45); color: white;" onclick="donationformVue.validateStep(\'step2\')">iDeal</button></span>');
 	   checkExist = setInterval(function() {
 		   if ($('.wizard-footer-left > span > button').length) {
 			   $('.wizard-footer-left > span > button').attr('onClick', 'resetNextBtn();');
@@ -63,5 +63,6 @@ function removeIdealBtn() {
 }
 
 function idealTransaction(data) {
-	console.log(data);
+	data = donationformVue._data.finalModel
+	console.log(data.marketingcode);
 }
