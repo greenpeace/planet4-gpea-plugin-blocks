f12d11447034db93670ffb6f9cb45a6b620ec25e
Oscar Keur <o.e.keur@gmail.com>
2019-03-12 13:55:20 +0100

    BUG: Remove jquery ui dep for onload.js
    Instead use the already include version, to prevent versions collisions

--- classes/controller/blocks/class-petition-controller.php
+++ classes/controller/blocks/class-petition-controller.php
@@ -310,8 +310,7 @@ if ( ! class_exists( 'Petition_Controller' ) ) {
 			}
 
 			//  Include the script and styling for the counter
-			wp_enqueue_script( 'petitioncounterjs', P4NLBKS_ASSETS_DIR . 'js/onload.js', [ 'jquery' ], '2.3.6', true );
-			wp_enqueue_script( 'jquery-ui', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js', [  'jquery'  ], '2.3.6', true );
+			wp_enqueue_script( 'petitioncounterjs', P4NLBKS_ASSETS_DIR . 'js/onload.js', [ 'jquery', 'jquery-effects-core' ], '2.5.1', true );
 			wp_enqueue_style( 'petitioncountercss', P4NLBKS_ASSETS_DIR . 'css/gpnl-petition.css', [], '2.3.6' );
 			wp_enqueue_script( 'clanglanding', P4NLBKS_ASSETS_DIR . 'js/clang-landing.js', ['jquery'], '2.3.6', true );
 
