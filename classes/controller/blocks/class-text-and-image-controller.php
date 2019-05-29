3c800b515e20e04a00eaf0f542e87e76f2462e89
nicklem <nicola.klemenc@gmail.com>
2019-05-29 17:40:30 +0200

    8# Fix backend

--- classes/controller/blocks/class-text-and-image-controller.php
+++ classes/controller/blocks/class-text-and-image-controller.php
@@ -1,12 +1,14 @@
 <?php
+/**
+ * Free text/image block class
+ *
+ * @package P4EABKS
+ * @since 0.1
+ */
 
 namespace P4EABKS\Controllers\Blocks;
 
 if ( ! class_exists( 'Text_And_Image_Controller' ) ) {
-	/**
-	 * @noinspection AutoloadingIssuesInspection
-	 */
-
 	/**
 	 * Class Text_And_Image_Controller
 	 *
@@ -14,10 +16,18 @@ if ( ! class_exists( 'Text_And_Image_Controller' ) ) {
 	 */
 	class Text_And_Image_Controller extends Controller {
 
-		/** @const string BLOCK_NAME */
+		/**
+		 * The block name constant.
+		 *
+		 * @const string BLOCK_NAME
+		 */
 		const BLOCK_NAME = 'text_and_image';
 
-		/** @const string DEFAULT_LAYOUT */
+		/**
+		 * The block default layout.
+		 *
+		 * @const string BLOCK_NAME
+		 */
 		const DEFAULT_LAYOUT = 'light';
 
 		/**
@@ -28,32 +38,6 @@ if ( ! class_exists( 'Text_And_Image_Controller' ) ) {
 
 			$fields = [
 				[
-					'label' => __( 'Title', 'planet4-gpea-blocks' ),
-					'attr'	=> 'title',
-					'type'	=> 'text',
-					'meta'	=> [
-						'placeholder' => __( 'Title', 'planet4-gpea-blocks' ),
-						'data-plugin' => 'planet4-gpea-blocks',
-					],
-				],
-				[
-					'label' => __( 'Text', 'planet4-gpea-blocks' ),
-					'attr'	=> 'text',
-					'type'	=> 'textarea',
-					'meta'	=> [
-						'placeholder' => __( 'Text', 'planet4-gpea-blocks' ),
-						'data-plugin' => 'planet4-gpea-blocks',
-					],
-				],
-				[
-					'label' => __( 'Optional image', 'planet4-gpea-blocks' ),
-					'attr'		  => 'img',
-					'type'		  => 'attachment',
-					'libraryType' => array( 'image' ),
-					'addButton'	  => __( 'Select image', 'planet4-gpea-blocks' ),
-					'frameTitle'  => __( 'Select image', 'planet4-gpea-blocks' ),
-				],
-				[
 					'label' => 'Select the layout',
 					'description' => 'Select the layout',
 					'attr' => 'layout',
@@ -61,32 +45,77 @@ if ( ! class_exists( 'Text_And_Image_Controller' ) ) {
 					'options' => [
 						[
 							'value' => 'light',
-							'label' => __( 'Light background, dark text', 'planet4-gpea-blocks' ),
-							'desc'	=> 'Light background, dark text',
+							'label' => __( 'Light background, dark text - issue color', 'planet4-gpea-blocks' ),
+							'desc'  => 'Dark text on light background. The main color changes, based on the issue.',
 							'image' => esc_url( plugins_url() . '/planet4-gpea-plugin-blocks/admin/img/latte.png' ),
 						],
 						[
 							'value' => 'dark',
-							'label' => __( 'Dark background, light text', 'planet4-gpea-blocks' ),
-							'desc'	=> 'Dark background, light text',
+							'label' => __( 'Dark background, light text - issue color', 'planet4-gpea-blocks' ),
+							'desc'  => 'Dark text on light background. The main color changes, based on the issue.',
 							'image' => esc_url( plugins_url() . '/planet4-gpea-plugin-blocks/admin/img/latte.png' ),
 						],
 						[
 							'value' => 'plain_light',
-							'label' => __( 'Plain text, light', 'planet4-gpea-blocks' ),
-							'desc'	=> 'Dark background, light text',
+							'label' => __( 'White background, black', 'planet4-gpea-blocks' ),
+							'desc'  => 'Black text on white background.',
 							'image' => esc_url( plugins_url() . '/planet4-gpea-plugin-blocks/admin/img/latte.png' ),
 						],
 					],
+				],
+				[
+					'label' => __( 'Title', 'planet4-gpea-blocks' ),
+					'attr'  => 'title',
+					'type'  => 'text',
+					'meta'  => [
+						'placeholder' => __( 'Title', 'planet4-gpea-blocks' ),
+						'data-plugin' => 'planet4-gpea-blocks',
+					],
+				],
+				[
+					'label' => __( 'Paragraph', 'planet4-gpea-blocks' ),
+					'attr'  => 'paragraph',
+					'type'  => 'textarea',
+					'meta'  => [
+						'placeholder' => __( 'Paragraph', 'planet4-gpea-blocks' ),
+						'data-plugin' => 'planet4-gpea-blocks',
+					],
+				],
+				[
+					'label' => __( 'Link label', 'planet4-gpea-blocks' ),
+					'attr'  => 'link_label',
+					'type'  => 'text',
+					'meta'  => [
+						'placeholder' => __( 'Link label', 'planet4-gpea-blocks' ),
+						'data-plugin' => 'planet4-gpea-blocks',
+					],
+				],
+				[
+					'label' => __( 'Link URL', 'planet4-gpea-blocks' ),
+					'attr'  => 'link_url',
+					'type'  => 'url',
+					'meta'  => [
+						'placeholder' => __( 'Link URL', 'planet4-gpea-blocks' ),
+						'data-plugin' => 'planet4-gpea-blocks',
+					],
+				],
+				[
+					'label' => __( 'Optional image(s)', 'planet4-gpea-blocks' ),
+					'attr'        => 'images',
+					'type'        => 'attachment',
+					'libraryType' => array( 'image' ),
+					'multiple'    => true,
+					'addButton'   => __( 'Select image(s)', 'planet4-gpea-blocks' ),
+					'frameTitle'  => __( 'Select image(s)', 'planet4-gpea-blocks' ),
 				],
 			];
 
 			// Define the Shortcode UI arguments.
 			$shortcode_ui_args = [
-				'label'			=> __( 'LATTE | Text and image block', 'planet4-gpea-blocks' ),
+				'label'         => __( 'Free text/image', 'planet4-gpea-blocks' ),
 				'listItemImage' => '<img src="' . esc_url( plugins_url() . '/planet4-gpea-plugin-blocks/admin/img/latte.png' ) . '" />',
-				'attrs'			=> $fields,
-				'post_type'		=> P4EABKS_ALLOWED_PAGETYPE,
+				'attrs'         => $fields,
+				'post_type'     => P4EABKS_ALLOWED_PAGETYPE,
 			];
 
 			shortcode_ui_register_for_shortcode( 'shortcake_' . self::BLOCK_NAME, $shortcode_ui_args );
@@ -96,7 +125,7 @@ if ( ! class_exists( 'Text_And_Image_Controller' ) ) {
 		/**
 		 * Get all the data that will be needed to render the block correctly.
 		 *
-		 * @param array	 $attributes This is the array of fields of this block.
+		 * @param array  $attributes This is the array of fields of this block.
 		 * @param string $content This is the post content.
 		 * @param string $shortcode_tag The shortcode tag of this block.
 		 *
@@ -104,8 +133,10 @@ if ( ! class_exists( 'Text_And_Image_Controller' ) ) {
 		 */
 		public function prepare_data( $attributes, $content = '', $shortcode_tag = 'shortcake_' . self::BLOCK_NAME ) : array {
 
-            if( isset( $attributes[ 'img' ] ) ) {
-				$attributes[ 'img' ] = wp_get_attachment_url( $attributes[ 'img' ] );
+			if ( isset( $attributes['images'] ) ) {
+				$attributes['images'] = array_map(function( $img_id ) {
+					return wp_get_attachment_url( $img_id );
+				}, explode( ',', $attributes['images'] ) );
 			}
 
 			$attributes['layout'] = isset( $attributes['layout'] ) ? $attributes['layout'] : self::DEFAULT_LAYOUT;
@@ -120,8 +151,8 @@ if ( ! class_exists( 'Text_And_Image_Controller' ) ) {
 		 * Callback for the shortcake_noindex shortcode.
 		 * It renders the shortcode based on supplied attributes.
 		 *
-		 * @param array	 $fields		Array of fields that are to be used in the template.
-		 * @param string $content		The content of the post.
+		 * @param array  $fields        Array of fields that are to be used in the template.
+		 * @param string $content       The content of the post.
 		 * @param string $shortcode_tag The shortcode tag (shortcake_blockname).
 		 *
 		 * @return string The complete html of the block
@@ -132,10 +163,7 @@ if ( ! class_exists( 'Text_And_Image_Controller' ) ) {
 
 			// Shortcode callbacks must return content, hence, output buffering here.
 			ob_start();
-
 			$this->view->block( self::BLOCK_NAME, $data );
-			// echo '<pre>' . var_export($data, true) . '</pre>';
-
 			return ob_get_clean();
 		}
 
