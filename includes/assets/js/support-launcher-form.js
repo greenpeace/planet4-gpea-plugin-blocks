04bf180389cd95ef105057bd88f235e780684b2e
Nicola Klemenc <nicola.klemenc@gmail.com>
2019-06-05 16:44:17 +0200

    Point 4: https://gitlab.com/nicklem1/344_gp_blocks_hongkong/issues/22#note_178103692

---
+++ includes/assets/js/support-launcher-form.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function() {
+  $('.form-support-launcher').on('submit', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var actionName = 'supportLauncher';
+    var $this = jQuery(this);
+    var messageBox = $('.form-support-launcher-message');
+    var destination = $this.find('input[name=send_to]').val();
+
+    var data = $this.serializeArray(),
+        payload = {
+          action: actionName,
+        };
+
+    for( var i = 0, len = data.length; i < len; i++ ) {
+      if(data[i].name !== actionName) {
+        payload[data[i].name] = data[i].value;
+      }
+    }
+
+    if( payload && destination ) {
+      jQuery.ajax(
+        destination,
+        {
+          data: payload,
+          method: 'POST',
+        }
+      ).done(function(response) {
+        messageBox.html(response);
+      });
+    }
+
+    return false;
+
+  });
+});
