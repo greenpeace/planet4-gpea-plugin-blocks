d341ec8e75c4756acb5a4f8985c40e86a905d1c5
Alessandro Tilli <alessandro@lattecreative.com>
2019-06-26 18:12:22 +0200

    Naming of testimonial and people

--- classes/controller/blocks/class-testimonials-controller.php
+++ classes/controller/blocks/class-testimonials-controller.php
@@ -130,7 +130,7 @@ if ( ! class_exists( 'Testimonials_Controller' ) ) {
 
 			// Define the Shortcode UI arguments.
 			$shortcode_ui_args = [
-				'label'         => __( 'GPEA | Testimonials', 'planet4-gpea-blocks' ),
+				'label'         => __( 'GPEA | People', 'planet4-gpea-blocks' ),
 				'listItemImage' => '<img src="' . esc_url( plugins_url() . '/planet4-gpea-plugin-blocks/admin/img/latte.png' ) . '" />',
 				'attrs'         => $fields,
 				'post_type'     => P4EABKS_ALLOWED_PAGETYPE,
