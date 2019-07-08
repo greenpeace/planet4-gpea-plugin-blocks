b1198e4065fb609706f8100ac61512ee8aa3c1be
Alessandro Tilli <alessandro@lattecreative.com>
2019-07-08 17:13:18 +0200

    lexicon added

--- classes/controller/blocks/class-tips-controller.php
+++ classes/controller/blocks/class-tips-controller.php
@@ -213,6 +213,7 @@ if ( ! class_exists( 'Tips_Controller' ) ) {
 
 			$lexicon = [
 				'tip_cta' => __( 'I\'ll do it!', 'planet4-gpea-blocks' ),
+				'committed' => __( 'Committed', 'planet4-gpea-blocks' ),
 			];
 
 			return [
