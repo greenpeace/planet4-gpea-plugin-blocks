d7c927e828a2765f273d80c51fda2f0d2870b0e9
Jason <>
2020-05-16 16:02:37 +0800

    add video control function

--- includes/assets/js/video_row.js
+++ includes/assets/js/video_row.js
@@ -5,31 +5,77 @@ window.onYouTubeIframeAPIReady = function() {
 };
 
 jQuery(document).ready(function() {
-    console.log('video row', YT);
+    var $ = jQuery;
     var player = {};
+    const classIsPlaying = 'is-playing';
+    const classIsAutoPlay = 'is-auto-play';
+    const classIsStopped = 'is-stopped';
+    const srcVideo = 'video';
+    const srcYoutube = 'youtube';
+
+    function bindButton(){
+        $('.video-row-play').click(function(e){
+            const parent = $(this).parents('.gpea-video-row-wrapper-full');
+            const id = parent.attr('data-id');
+            const src = parent.attr('src-type');
+            parent.removeClass(classIsStopped).removeClass(classIsAutoPlay);
+            parent.addClass(classIsPlaying);
+            switch(src){
+                case srcVideo: 
+                    var video = document.getElementById(id);
+                    video.muted = false;
+                    video.currentTime = 0.1;
+                    video.play();
+                    break;
+                case srcYoutube:
+                    player[id].unMute();
+                    player[id].seekTo(0);
+                    player[id].playVideo();
+                    break;
+            }
+        });
+        $('.video-row-stop').click(function(e){
+            const parent = $(this).parents('.gpea-video-row-wrapper-full');
+            const id = parent.attr('data-id');
+            const src = parent.attr('src-type');
+            parent.removeClass(classIsPlaying).removeClass(classIsAutoPlay);
+            parent.addClass(classIsStopped);
+            switch(src){
+                case srcVideo: 
+                    document.getElementById(id).pause();
+                    break;
+                case srcYoutube:
+                    player[id].pauseVideo();
+                    break;
+            }
+        });
+    }
+    //init youtube
     YTdeferred.done(function(YT) {
         // use YT here
-        console.log(YT);
         jQuery('.video-row-youtube').each(function(idx, el){
-            console.log(el);
             const id = jQuery(this).attr('id');
+            const isAutoplay = jQuery(this)
+                                    .parent('.gpea-video-row-wrapper-full')
+                                    .attr('is-autoplay') || 0;
             player[id] = new YT.Player(id, {
                 width: '100%',
-                videoId: '668nUCeBHyY',
+                videoId: 'a3ICNMQW7Ok',
                 host: 'https://www.youtube-nocookie.com',
                 playerVars: {
-                    'autoplay': 1,
+                    'autoplay': isAutoplay,
                     'modestbranding': 1,
                     'autohide': 1,
                     'showinfo': 0,
                     'loop': 1,
                     'mute': 1,
                     'controls': 0,
+                    'rel': 0,
                 },
                 events: {
                     'onReady': function(){ },
                     'onStateChange': function onPlayerStateChange(event) {
-                        if (event.data == YT.PlayerState.PLAYING && !done) { }
+                        if (event.data == YT.PlayerState.PLAYING) { }
                         if (event.data === YT.PlayerState.ENDED) {
                             player[id].playVideo();
                         }
@@ -38,5 +84,7 @@ jQuery(document).ready(function() {
             });
         });
     });
+
+    bindButton();
 });
   
