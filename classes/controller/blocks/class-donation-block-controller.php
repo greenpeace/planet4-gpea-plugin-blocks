189cd1e85c72a144dc4cd5476263693a0fcae9d3
hit1205 <hit1205.tw@gmail.com>
2021-09-26 13:38:22 +0800

    Completed donation block for page

--- classes/controller/blocks/class-donation-block-controller.php
+++ classes/controller/blocks/class-donation-block-controller.php
@@ -36,6 +36,126 @@ if ( ! class_exists( 'Donation_Block_Controller' ) ) {
 		 */
 		public function prepare_fields() {
 
+			$fields = [
+				[
+					'label' => __( 'Title', 'planet4-gpea-blocks-backend' ),
+					'attr'  => 'title',
+					'type'  => 'text',
+					'meta'  => [
+						'placeholder' => __( 'Title', 'planet4-gpea-blocks-backend' ),
+						'data-plugin' => 'planet4-gpea-blocks',
+					],
+				],
+				[
+					'label'       => __( 'Button label', 'planet4-gpea-blocks-backend' ),
+					'description' => __( 'Leave empty to use default.', 'planet4-gpea-blocks-backend' ),
+					'attr'        => 'button_text',
+					'type'        => 'text',
+					'meta'        => [
+						'placeholder' => __( 'Button label', 'planet4-gpea-blocks-backend' ),
+						'data-plugin' => 'planet4-gpea-blocks',
+					],
+				],
+				[
+					'label'       => __( 'Button Link', 'planet4-gpea-blocks-backend' ),
+					'description' => __( 'Leave empty to use default.', 'planet4-gpea-blocks-backend' ),
+					'attr'        => 'button_link',
+					'type'        => 'url',
+					'meta'        => [
+						'placeholder' => __( 'Button Link', 'planet4-gpea-blocks-backend' ),
+						'data-plugin' => 'planet4-gpea-blocks',
+					],
+				],
+				[
+					'label' => __( 'Background Image', 'planet4-gpea-blocks-backend' ),
+					'attr'        => 'bg_img',
+					'type'        => 'attachment',
+					'libraryType' => array( 'image' ),
+					'addButton'   => __( 'Select image', 'planet4-gpea-blocks-backend' ),
+					'frameTitle'  => __( 'Select image', 'planet4-gpea-blocks-backend' ),
+				],				
+			];
+
+			// Define the Shortcode UI arguments.
+			$shortcode_ui_args = [
+				'label'             => __( 'GPEA | Donation Block', 'planet4-gpea-blocks-backend' ),
+				'listItemImage' => '<img src="' . esc_url( plugins_url() . '/planet4-gpea-plugin-blocks/admin/img/donation-block.jpg' ) . '" />',
+				'attrs'             => $fields,
+				'post_type'         => P4EABKS_ALLOWED_PAGETYPE,
+			];
+
+			shortcode_ui_register_for_shortcode( 'shortcake_' . self::BLOCK_NAME, $shortcode_ui_args );
+
+		}
+
+		/**
+		 * Get all the data that will be needed to render the block correctly.
+		 *
+		 * @param array  $attributes This is the array of fields of this block.
+		 * @param string $content This is the post content.
+		 * @param string $shortcode_tag The shortcode tag of this block.
+		 *
+		 * @return array The data to be passed in the View.
+		 */
+		public function prepare_data( $attributes, $content = '', $shortcode_tag = 'shortcake_' . self::BLOCK_NAME ) : array {
+
+			global $post;
+			if ( class_exists( 'P4CT_Site' ) ) {
+				$gpea_extra = new \P4CT_Site();
+				$main_issue = $gpea_extra->gpea_get_main_issue($post->ID);
+				if( $main_issue ) {
+					$main_issue_slug = $main_issue->slug;
+				}
+			}
+			
+			// Get block options
+
+			$donation_block_options = get_option( 'gpea_donation_block_options' );
+
+			$donation_block_default_title = '';
+			if( isset( $main_issue_slug ) && isset( $donation_block_options['gpea_donation_block_' . $main_issue_slug . '_title'] ) && @strlen( $donation_block_options['gpea_donation_block_' . $main_issue_slug . '_title'] ) ) {
+				$donation_block_default_title = $donation_block_options['gpea_donation_block_' . $main_issue_slug . '_title'];
+			}
+			elseif( isset( $donation_block_options['gpea_donation_block_default_title'] ) ) {
+				$donation_block_default_title = $donation_block_options['gpea_donation_block_default_title'];
+			}
+
+			$donation_block_default_link = '';
+			if( isset( $main_issue_slug ) && isset( $donation_block_options['gpea_donation_block_' . $main_issue_slug . '_button_link'] ) && @strlen( $donation_block_options['gpea_donation_block_' . $main_issue_slug . '_button_link'] ) ) {
+				$donation_block_default_link = $donation_block_options['gpea_donation_block_' . $main_issue_slug . '_button_link'];
+			}
+			elseif( isset( $donation_block_options['gpea_donation_block_default_button_link'] ) ) {
+				$donation_block_default_link = $donation_block_options['gpea_donation_block_default_button_link'];
+			}
+
+			$donation_block_default_text = '';
+			if( isset( $main_issue_slug ) && isset( $donation_block_options['gpea_donation_block_' . $main_issue_slug . '_button_text'] ) && @strlen( $donation_block_options['gpea_donation_block_' . $main_issue_slug . '_button_text'] ) ) {
+				$donation_block_default_text = $donation_block_options['gpea_donation_block_' . $main_issue_slug . '_button_text'];
+			}
+			elseif( isset( $donation_block_options['gpea_donation_block_default_button_text'] ) ) {
+				$donation_block_default_text = $donation_block_options['gpea_donation_block_default_button_text'];
+			}
+
+			if ( !isset( $attributes['title'] ) || !@strlen( $attributes['title'] ) ) {
+				$attributes['title'] = $donation_block_default_title;
+			}
+
+			if ( !isset( $attributes['button_link'] ) || !@strlen( $attributes['button_link'] ) ) {
+				$attributes['button_link'] = $donation_block_default_link;
+			}
+
+			if ( !isset( $attributes['button_text'] ) || !@strlen( $attributes['button_text'] ) ) {
+				$attributes['button_text'] = $donation_block_default_text;
+			}
+
+			if ( isset( $attributes['bg_img'] ) ) {
+				$attributes['bg_img'] = wp_get_attachment_url( $attributes['bg_img'] );
+			}
+
+			return [
+				'fields' => $attributes,
+			];
+
 		}
 
 		/**
