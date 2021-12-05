9ffc91c424c895ba11104963188a4b861bfad712
hit1205 <hit1205.tw@gmail.com>
2021-12-05 13:33:17 +0800

    Completed Homepage B Version 2nd screen block

--- planet4-gpea-blocks.php
+++ planet4-gpea-blocks.php
@@ -146,6 +146,7 @@ P4EABKS\Loader::get_instance(
 		'P4EABKS\Controllers\Blocks\MC_Subscription_Controller',
 		'P4EABKS\Controllers\Blocks\Grid_Images_Controller',
 		'P4EABKS\Controllers\Blocks\Donation_Block_Controller',
+		'P4EABKS\Controllers\Blocks\Homepage_B2_Controller',
 		'P4EABKS\Models\Taxonomy',
 		'P4EABKS\Models\Tags_Meta',
 	], 'P4EABKS\Views\View'
