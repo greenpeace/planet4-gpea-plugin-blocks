94bf5497a947f3b42e79de95c50894777ec491f6
hit1205 <hit1205.tw@gmail.com>
2022-09-30 21:55:23 +0800

    Increase "World Slideshow" block's limit to the number of items, from 20 to 35

--- classes/controller/blocks/class-world-slideshow-controller.php
+++ classes/controller/blocks/class-world-slideshow-controller.php
@@ -29,7 +29,7 @@ if ( ! class_exists( 'World_Slideshow_Controller' ) ) {
 		 *
 		 * @const string MAX_REPEATER
 		 */
-		const MAX_REPEATER = 20;
+		const MAX_REPEATER = 35;
 
 		/**
 		 * Shortcode UI setup for the tasks shortcode.
